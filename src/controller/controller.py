1e19c730f8b4a7152525947ede07cfd52ad0b7a8
Dmitry Mordanov <mordanov314@gmail.com>
2022-12-09 14:55:42 +0300

    initial commit

---
+++ src/controller/controller.py
@@ -0,0 +1,14 @@
+#from src.view.view import Viewer
+#from src.data.data import Data
+
+
+class Controller:
+    # Контроллер - тут все происходит
+#    viewer = Viewer()
+#    data = Data()
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        print("Hi from Reversi!")
---
+++ src/main.py
@@ -0,0 +1,8 @@
+from src.controller.controller import Controller
+
+# Необходимо реализовать свой проект на каком-то другом языке программирования
+
+# Игра Реверси
+if __name__ == '__main__':
+    controller = Controller()
+    controller.run()
